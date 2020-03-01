254bb6719bad662493ad7e27961f104076c3ed31
sarveshhome <sarvesh.carlton@gmail.com>
2020-03-01 13:06:11 +0530

    index page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { PageHeaderComponent } from '@src/app/core/page-header/page-header.component';
+import { PageFooterComponent } from '@src/app/core/page-footer/page-footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    PageHeaderComponent,
+    PageFooterComponent,
   ],
   imports: [
     BrowserModule,
